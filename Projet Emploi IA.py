a43ec9a99055e44eec749b0ab1e954624faa56b3
ernoux41m <matt.er41@hotmail.fr>
2023-01-26 15:03:50 +0100

    Merge main/Clementine (#9)
    * Salaire nom et date
    
    * Compétences nettoyage
    
    * Commit du csv
    
    * Count vectorize + premières question d'analyses
    
    * Creation colonnes "Intitule" et "Type_poste"
    
    * Preprocess
    
    * Recupération
    
    * Fusion avec clémentine
    
    * encodage
    
    Co-authored-by: SylvineDurand <durand.sylvine@gmail.com>
    Co-authored-by: SylvineDurand <120107600+SylvineDurand@users.noreply.github.com>

--- Projet Emploi IA.py
+++ Projet Emploi IA.py
@@ -1,3 +1,4 @@
+# -- coding: utf-8 --
 # Projet emploi IA
 # Clémentine, Matthieu, Sylvine
 
@@ -6,63 +7,208 @@
 
 # import librairies
 import pandas as pd
+import numpy as np
+from datetime import timedelta
+from sklearn.feature_extraction.text import CountVectorizer
+
 
 # Ouverture dataset
 df = pd.read_json("https://raw.githubusercontent.com/SylvineDurand/Projet-emploi-IA-Cl-mentine-Matthieu-Sylvine/main/data.json")
 
 
-# NETTOYAGE DONNEES
-type(df.iloc[[0],[0]]) # est un df 
-type(df.iloc[[0],[0]].values[0]) # np.ndarray
-type(df.iloc[[0],[0]].values[0][0]) # list
-type(df.iloc[[0],[0]].values[0][0][0]) # str
-
-# Création de la colonne Intitulé
-# pour test sur 1 seul élément
-test = df.iloc[[0],[0]].values[0][0][0]
-type(test)
-test
-
-# Liste de caractères �  enlever
+# I. NETTOYAGE DONNEES
+# 1. Création de la colonne Intitule
+# Liste d'éléments à retirer notamment au milieu des strings
 to_remove = ["[","]","'","(",")", 
-             "H/F","F/H","h/f","f/h"
-             ]
+             "H/F","F/H","h/f","f/h"]
 
-# pour test sur 1 seul élément
-test = df.iloc[[0],[0]].values[0][0][0]
-type(test)
-test  
+# Liste d'éléments à retirer au début des strings
+debut_to_remove = ["2019-moa-data-30912",
+                   "2018-788",
+                   "2016-433"]
 
-for i in to_remove:
-    test = test.replace(i,"")
-# on split sur le tiret
-test = test.split(" - ")[0]
-# on strip espace et antislash n
-test.strip("").strip("\n")    
+contract = ["stage",
+            "stagiaire",
+            "cdi",
+            "apprenti",
+            "alternant",
+            "alternance"]               
 
-# Fonction �  appliquer sur la colonne "intitulé de poste" pour nettoyer
-def func_intitule(x):
+## Première fonction enlève les caractères sans utilité
+def f1(x):
     x = x[0]
+    x = x.lower()
+    for i in to_remove:
+        x = x.replace(i,"")    
+    for i in debut_to_remove:
+        x = x.replace(i,"")      
+    x = x.strip(" \n-")
+    return x
+
+# créer colonne intermediaire
+df["Int1"] = df["Intitulé du poste"].apply(f1)  
+
+# 2e fonction récup l'info alternance pour utilisation ultérieure
+def f2(x):
+    y = ""
+    for i in contract:
+        if x.startswith(i):
+            y =  y + i
+    return y   
+
+# créer colonne intermediaire, sera utilisée plus tard
+df["Int2"] = df["Int1"].apply(f2)  
+
+# 3e fonction enlève info stage etc, enleve caractères restant au début, récup première partie
+def f3(x):
+    for i in contract:
+        x = x.replace(i,"") 
+    x = x.strip(" -–:e")
     x = x.split(" - ")[0]
     x = x.split(" – ")[0]
     x = x.split(" / ")[0]
-    for i in to_remove:
-        x = x.replace(i,"")    
-    x = x.strip("").strip("\n").strip("-")
     return x
-    
-func_intitule(test)
-    
-df["Intitule"] = df["Intitulé du poste"].apply(func_intitule)    
-    
-# reste �  faire: 
-    # - index 13 = alternant en CDI?
-    # - 34 alternance (contrat pro ok)
-    # - 49 stage chargé projet en CDI
-    # - 55 commence par chiffre
-    # - 139 2019 moa data etc*
-    # - 185 Assistant comptable???
+
+# Créer colonne finale    
+df["Intitule"] = df["Int1"].apply(f3)  
 
 
-    
+# 2. Creation colonne Type_poste
+def type_contrat(x):
+    y = "raté"
+    if x[0] == '':
+        y = x[1][-1].split(" - ")[0],
+    elif x[0] == 'stage':
+        y = "Stage"
+    elif x[0] == 'stagiaire':
+        y = "Stage"
+    elif x[0] == 'alternant':
+        y = "Alternance"
+    elif x[0] == 'alternance':
+        y = "Alternance"
+    elif x[0] == 'apprenti':
+        y = "Apprentissage"
+    elif x[0] == 'cdi':
+        y = "cdi"
+    return y
 
+df['Type_poste'] = df[["Int2","Type de poste"]].apply(lambda x : type_contrat(x), axis = 1)
+type(df.Type_poste[0]) # est un tuple, il faut le convertir
+
+# Conversion des tuples en str
+def convertTuple(tup):
+    st = ''.join(map(str, tup))
+    return st
+ 
+df['Type_poste'] = df['Type_poste'].apply(lambda x : convertTuple(x))
+type(df.Type_poste)  # series
+
+# remove les \n
+df['Type_poste'] = df['Type_poste'].apply(lambda x : x.strip("\n",).lower())
+
+
+# 3. Creation colonne date
+#fonction pour la date et l'apply avec 
+def date(df):
+    df = df.strip("").strip("\n")
+    df = df.split(" ")
+    if df[-1] == "hier" or df[-1] == "heures":
+        df = pd.to_datetime("2023/01/14")
+    else:
+        df = df[-2:]
+        if df[-1] == "mois":
+            x= int(df[-2])
+            temps=x*31
+            df = pd.to_datetime("2023/01/15")-timedelta(days=temps)
+        else:
+            temps=int(df[-2])
+            df = pd.to_datetime("2023/01/15")-timedelta(days=temps)
+    
+        
+    return df
+
+df["Date de publication"] = df["Date de publication"].apply(date)
+
+# 4. Creation colonne Société
+#fonction pour mise en miniscule pour les noms de sociétés
+def nom(df):
+    df = df[2].lower()
+    
+    return df
+
+df["Type de poste"] = df["Type de poste"].apply(nom)
+
+data = df['lieu']
+Lieu=[]
+for i in data:
+    list_lieu=[]
+    for c in i[0] :
+      list_lieu.append(c.replace('\n', ''))
+    Lieu.append(' '.join(list_lieu))
+
+df['lieu'] = Lieu
+
+# 5. Creation colonnes salaire
+#fonction de salaire qui garde le salaire 
+def salaire(df):
+    if len(df) == 2:
+        df = df[1]
+        df = df.split("/ an")[0]
+        df = df.split("/an")[0]
+        df = df.replace("€","").replace(".","").replace(",00","")
+          
+    else:
+        df = "NaN"
+    
+    return df
+
+df["Salaire"] = df["lieu"].apply(salaire)
+
+
+#fonctions qui font le salaire max et min en integer
+def salaire_min(df):
+    df = df.split("-")[0]
+    if df != "NaN":
+        df = int(df)
+    return df
+def salaire_max(df):
+    df = df.split("-")[-1]
+    if df != "NaN":
+        df = int(df)
+    return df
+
+df["Salaire_minimum"] = df["Salaire"].apply(salaire_min)
+df["Salaire_maximum"] = df["Salaire"].apply(salaire_max)
+df=df.drop(["Salaire"],axis=1)
+
+# 6. Creation colonne compétences
+#join les éléments dans chaque liste de compétences en les changeant en string 
+df['competences'] = [', '.join(map(str, l)) for l in df['competences']]
+
+
+#retire les retours à la ligne des compétences 
+def retour_a_la_ligne(value):
+    return ''.join(value.splitlines())
+
+df["competences"] = df["competences"].apply(retour_a_la_ligne)
+
+# II. Préprocessing des données
+# 1. Count vectorize compétences
+#count-vectorize pour les compétences qui tranforme le nombre de mots en 1 utiliser dans un array
+vectorizer = CountVectorizer()
+X = vectorizer.fit_transform(df["competences"])
+vectorizer.get_feature_names_out()
+
+
+# III. Analyse exploratoire
+# 1. Entreprises qui embauchent le plus
+#compte le nombre de valeurs d'entreprise dans la colonne type de poste
+pd.Series(df["Type de poste"]).value_counts()
+
+
+print(X)
+
+
+
+
+df.to_csv("test.csv")
